b5ae202711d728ee5508edff1215a0446686ab6e
ormil400-commits <ormil400@gmail.com>
2026-06-13 12:58:24 +0200

    connexió amb un servidor

--- Main.py
+++ Main.py
@@ -3,3 +3,26 @@ import random
 import json
 import pygame
 
+
+# inici servidor
+resposta = requests.get("fun.codelearn.cat/hackathon/game/new")
+info_inici = resposta.json()
+
+game_id = info_inici["game_id"]
+seed = info_inici["seed"]
+
+random.seed(seed)#Inicialitza la random amb la seed del servidor 
+    
+
+score = 0 # inicialitza puntuació
+invent-actual = #anything, fes-ho tu perq agafi un invent aleatori
+
+#progres
+info_progres = {"game_id": game_id, "data": invent-actual, "score": score}
+headers = {}
+resposta_progres = requests.post("fun.codelearn.cat/hackathon/game/store_progress", headers=headers)
+    
+
+#envia al servidor la puntuació
+info_fi = {"game_id": game_id, "data": "ha fallat", "score": score}
+resposta_fi = requests.post("fun.codelearn.cat/hackathon/game/finalize", json=send)
